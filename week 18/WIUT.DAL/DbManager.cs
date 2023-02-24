a19d1da739637454f97d3f2a79ebd46d5b29a1d9
Sherzodbek1717 <113887151+Sherzodbek1717@users.noreply.github.com>
2023-02-24 03:56:23 -0600

    02/24

---
+++ week 18/WIUT.DAL/DbManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace WIUT.DAL
+{
+    public class DbManager
+    {
+        protected SQLiteConnection Connection
+        {
+            get
+            {
+                return new SQLiteConnection(Properties.Settings.Default.ConnectionString);
+            }
+        }
+    }
+}
---
+++ week 18/WIUT/ApplicantListForm.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WIUT.DAL;
+
+namespace WIUT
+{
+    public partial class ApplicantListForm : Form
+    {
+        public ApplicantListForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ApplicantListForm_Load(object sender, EventArgs e)
+        {
+            MdiParent = MyForms.GetForm<ParentForm>();
+            LoadData();
+
+        }
+
+        private void LoadData()
+        {
+            dgv.DataMember = "";
+            dgv.DataSource = null;
+            dgv.DataSource = new ApplicantManager().GetAll();
+
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            if (cbxSort.SelectedIndex < 0)
+                MessageBox.Show("Please select an option to sort by");
+            else
+            {
+                ByAttribute selectedAttribute;
+                if (cbxSort.SelectedIndex == 0)
+                    selectedAttribute = ByAttribute.Name;
+                else if (cbxSort.SelectedIndex == 1)
+                    selectedAttribute = ByAttribute.Surname;
+                else if (cbxSort.SelectedIndex == 2)
+                    selectedAttribute = ByAttribute.DoB;
+                else
+                    selectedAttribute = ByAttribute.Course;
+
+                dgv.DataMember = "";
+                dgv.DataSource = null;
+                dgv.DataSource = new ApplicantManager().Sort(selectedAttribute);
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+
+            if (cbxSearch.SelectedIndex < 0)
+                MessageBox.Show("Selected an option to search by");
+            else if (string.IsNullOrWhiteSpace(tbxSearch.Text))
+                MessageBox.Show("please supply search term");
+            else
+            {
+                var selectedAttribute = cbxSearch.SelectedIndex == 0 ? ByAttribute.Name : ByAttribute.Surname;
+                dgv.DataMember = "";
+                dgv.DataSource = null;
+                dgv.DataSource = new ApplicantManager().Search(tbxSearch.Text, selectedAttribute);
+
+            }
+        }
+    }
+}
